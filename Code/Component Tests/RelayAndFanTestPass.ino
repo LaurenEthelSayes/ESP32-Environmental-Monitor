3b89bb2defb6edf877ce6c4d7570b5aadfbb0797
Lauren <LaurenEthelSayers@gmail.com>
2024-12-17 01:02:00 +0000

    Add files via upload

---
+++ Code/Component Tests/RelayAndFanTestPass.ino
@@ -0,0 +1,17 @@
+#define RELAY_PIN 14
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Relay Module Test Starting...");
+}
+
+void loop() {
+  Serial.println("Relay ON - Fan ON");
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(2000);
+
+  Serial.println("Relay OFF - Fan OFF");
+  digitalWrite(RELAY_PIN, LOW);
+  delay(2000);
+}
